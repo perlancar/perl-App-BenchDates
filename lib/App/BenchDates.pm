516f21d224c1f37dab4d86610555553c2c398219
perlancar (@pc-office) <perlancar@gmail.com>
2015-08-06 17:30:22 +0700

    Initial commit

---
+++ lib/App/BenchDates.pm
@@ -0,0 +1,18 @@
+package App::BenchDates;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Benchmark date-/time-related operations
+
+=head1 SYNOPSIS
+
+See the included scripts:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<App::BenchDateModules>
